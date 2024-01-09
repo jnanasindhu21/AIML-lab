2498935c233c8efd62c16ed2cf7bba9493a80675
Jnana Sindhu K S <96429100+jnanasindhu21@users.noreply.github.com>
2024-01-09 13:09:53 +0530

    Update Lab4.py

--- Lab4.py
+++ Lab4.py
@@ -1,74 +1,90 @@
 from pprint import pprint
 import pandas as pd
+from pandas import DataFrame
 
 df_tennis = pd.read_csv('Lab4.csv')
+
 df_tennis = pd.read_csv('Lab4.csv')
 
 def entropy(probs):
     import math
     return sum([-prob * math.log(prob, 2) for prob in probs])
 
-def entropy_of_list(a_list):
+def entropy_of_list(a_list):  # Entropy calculation of list of discrete val ues(YES / NO)
     from collections import Counter
     cnt = Counter(x for x in a_list)
+    print("No and Yes Classes:", a_list.name, cnt)
     num_instances = len(a_list) * 1.0
     probs = [x / num_instances for x in cnt.values()]
-    return entropy(probs)
-
-total_entropy = entropy_of_list(df_tennis['PlayTennis'])
+    return entropy(probs)  # Call Entropy:
 
 def information_gain(df, split_attribute_name, target_attribute_name, trace=0):
-    
     df_split = df.groupby(split_attribute_name)
+
+    for name, group in df_split:
+        print(name)
+        print(group)
+
     nobs = len(df.index) * 1.0
-    df_agg_ent = df_split.agg({target_attribute_name: [entropy_of_list, lambda x: len(x) / nobs]})[target_attribute_name]
+
+    df_agg_ent = df_split.agg({target_attribute_name: [entropy_of_list, lambda x: len(x) / nobs]})[
+        target_attribute_name]
+
     df_agg_ent.columns = ['Entropy', 'PropObservations']
+
     new_entropy = sum(df_agg_ent['Entropy'] * df_agg_ent['PropObservations'])
     old_entropy = entropy_of_list(df[target_attribute_name])
     return old_entropy - new_entropy
 
-def id3(df, target_attribute_name, attribute_names, default_class=None):
-    
+def id3(df, target_attribute_name, attribute_names, default_class=None):  # Tally target attribute
     from collections import Counter
-    cnt = Counter(x for x in df[target_attribute_name])
-    
+    cnt = Counter(x for x in df[target_attribute_name])  # class of YES /NO
+
     if len(cnt) == 1:
         return next(iter(cnt))
     elif df.empty or (not attribute_names):
         return default_class
     else:
         default_class = max(cnt.keys())
-        gainz = [information_gain(df, attr, target_attribute_name) for attr in attribute_names]
+
+        gainz = [information_gain(df, attr, target_attribute_name)
+                 for attr in attribute_names]
         index_of_max = gainz.index(max(gainz))
         best_attr = attribute_names[index_of_max]
+
         tree = {best_attr: {}}
-        remaining_attribute_names = [i for i in attribute_names if i != best_attr]
-        
+        remaining_attribute_names = [
+            i for i in attribute_names if i != best_attr]
+
         for attr_val, data_subset in df.groupby(best_attr):
-            subtree = id3(data_subset, target_attribute_name, remaining_attribute_names, default_class)
+            subtree = id3(data_subset, target_attribute_name,
+                          remaining_attribute_names, default_class)
             tree[best_attr][attr_val] = subtree
         return tree
 
+# Predicting Attributes
 attribute_names = list(df_tennis.columns)
-attribute_names.remove('PlayTennis')
+attribute_names.remove('PlayTennis')  # Remove the class attribute
 
+# Tree Construction
 tree = id3(df_tennis, 'PlayTennis', attribute_names)
 print("\n\nThe Resultant Decision Tree is :\n")
 pprint(tree)
 
+# Classification Accuracy
 def classify(instance, tree, default=None):
-    
-    attribute = next(iter(tree))
-    
+    attribute = next(iter(tree))  # tree.keys()[0]
     if instance[attribute] in tree[attribute].keys():
         result = tree[attribute][instance[attribute]]
-        if isinstance(result, dict):
+        if isinstance(result, dict):  
             return classify(instance, result)
         else:
-            return result
+            return result  
     else:
         return default
 
 df_tennis['predicted'] = df_tennis.apply(classify, axis=1, args=(tree, 'No'))
-print('Accuracy is:' + str(sum(df_tennis['PlayTennis'] == df_tennis['predicted']) / (1.0 * len(df_tennis.index))))
+
+print('Accuracy is:' + str(sum(df_tennis['PlayTennis'] ==
+      df_tennis['predicted']) / (1.0 * len(df_tennis.index))))
 df_tennis[['PlayTennis', 'predicted']]
